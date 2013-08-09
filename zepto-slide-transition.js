d354fa48c3bf6f895b6f3875b836951b60e97515
Davi Ferreira <davi.ferreira@corp.globo.com>
2013-08-09 15:54:40 -0300

    Remove trailing spaces

--- zepto-slide-transition.js
+++ zepto-slide-transition.js
@@ -2,27 +2,27 @@
 (function ($) {
 
     /* SlideDown */
-    $.fn.slideDown = function (duration) {    
-    
+    $.fn.slideDown = function (duration) {
+
         // get the element position to restore it then
         var position = this.css('position');
-        
+
         // show element if it is hidden
         this.show();
-        
+
         // place it so it displays as usually but hidden
         this.css({
             position: 'absolute',
             visibility: 'hidden'
         });
-        
+
         // get naturally height, margin, padding
         var marginTop = this.css('margin-top');
         var marginBottom = this.css('margin-bottom');
         var paddingTop = this.css('padding-top');
         var paddingBottom = this.css('padding-bottom');
         var height = this.css('height');
-        
+
         // set initial css for animation
         this.css({
             position: position,
@@ -34,7 +34,7 @@
             paddingTop: 0,
             paddingBottom: 0
         });
-        
+
         // animate to gotten height, margin and padding
         this.animate({
             height: height,
@@ -43,27 +43,27 @@
             paddingTop: paddingTop,
             paddingBottom: paddingBottom
         }, duration);
-        
+
     };
 
     /* SlideUp */
     $.fn.slideUp = function (duration) {
-    
+
         // active the function only if the element is visible
         if (this.height() > 0) {
-        
+
             var target = this;
-            
+
             // get the element position to restore it then
             var position = target.css('position');
-            
+
             // get the element height, margin and padding to restore them then
             var height = target.css('height');
             var marginTop = target.css('margin-top');
             var marginBottom = target.css('margin-bottom');
             var paddingTop = target.css('padding-top');
             var paddingBottom = target.css('padding-bottom');
-            
+
             // set initial css for animation
             this.css({
                 visibility: 'visible',
@@ -74,7 +74,7 @@
                 paddingTop: paddingTop,
                 paddingBottom: paddingBottom
             });
-            
+
             // animate element height, margin and padding to zero
             target.animate({
                 height: 0,
@@ -83,7 +83,7 @@
                 paddingTop: 0,
                 paddingBottom: 0
                 },
-                { 
+                {
                 // callback : restore the element position, height, margin and padding to original values
                 duration: duration,
                 queue: false,
@@ -102,14 +102,14 @@
             });
         }
     };
-    
+
     /* SlideToggle */
     $.fn.slideToggle = function (duration) {
-    
+
         // if the element is hidden, slideDown !
         if (this.height() == 0) {
             this.slideDown();
-        } 
+        }
         // if the element is visible, slideUp !
         else {
             this.slideUp();
